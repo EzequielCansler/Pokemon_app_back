3a2fe0083e766e08a8d3d0a5ef05f1f4fe7d7549
Ezequiel <ezequielcansler91@gmail.com>
2024-01-16 11:52:02 -0300

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
+import express, { static as expressStatic } from "express";
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(express.static('public'))
+app.use(expressStatic("public"));
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+app.get("*", (req, res) => {
+  res.redirect("/");
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  console.log(`App listening on port ${port}`);
+});
